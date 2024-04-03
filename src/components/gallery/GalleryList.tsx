219b000ff12a8222f6b2cc2ee319f02a527ac9aa
khoidang2110 <khoidang2110@gmail.com>
2024-04-03 17:42:00 +0700

    album title responsive

--- src/components/gallery/GalleryList.tsx
+++ src/components/gallery/GalleryList.tsx
@@ -1,4 +1,6 @@
-import { FC } from "react";
+'use client'
+
+import { FC, useEffect, useState } from "react";
 
 
 import styles from "./styles.module.css";
@@ -12,10 +14,29 @@ interface PostListTypeSmallProps {
 }
 const GalleryList: FC<PostListTypeSmallProps> = ({ projects = [],filterType }) => {
 
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+console.log(windowWidth)
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    setWindowWidth(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+
+
   const filteredData = projects.filter(item => item.type === filterType);
   return (
     <section>
-      <p className='pl-2  uppercase opacity-70'>{filteredData[0].type}   </p>
+ {windowWidth < 900 ? <p className='pl-2  uppercase opacity-70'>{filteredData[0].type}   </p> :""}
+
 <div  className={styles.galleryListSection}>
 {filteredData.reverse().map((item: IProject, index: number) => (
         <div className={styles.threePecent}>
