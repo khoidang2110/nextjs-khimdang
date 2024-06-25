a2e96d108adc4d93822622e263bd1379b39e0c2f
khoidang2110 <khoidang2110@gmail.com>
2024-06-25 11:26:33 +0700

    saigoneer

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,6 +9,9 @@ export default function About() {
 <AboutImg/>
      <div className="flex flex-col  items-center pt-3">
       <h1 className="text-xl">FEATURES</h1>
+
+      <a className=" underline underline-offset-1 " href="https://saigoneer.com/ton-sur-ton/27135-easybadwork-s-free-spirits-are-rooted-in-nature-and-the-underground?fbclid=IwZXh0bgNhZW0CMTEAAR0Z4KJMikozpKOB9k5-KZbv_enTIS_01ZqlwEtHsOpeCHpON_NXM9Lk5UM_aem_n5QZSwuxVZ_uUqQQbio06w">Saigoneer</a>
+
       <a className=" underline underline-offset-1 " href="https://vietcetera.com/vn/khim-dang-loi-the-lon-nhat-cua-nghe-si-viet-nam-la-can-tinh-viet">VietCetera</a>
     
       <a className=" underline underline-offset-1 " href="https://www.elle.vn/the-gioi-van-hoa/khim-dang-con-cop-khong-cua-rieng-ai">ELLE</a>
